bba37073a6099956012e2460a72d3e8aa77c47bf
Md Abbas Ali <mdabbasali185@gmail.com>
2022-04-12 16:06:35 +0600

    responsive check

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Service.css'
 
-const Service = () => {
+const Service = ({service}) => {
+    const { name,img,description,price}=service
     return (
-        <div>
-            
+        <div className='service'>
+          <h1>{name}</h1>  
+          <h2><img src={img} alt="img" /></h2>
+          <p>Price: {price}</p>
+          <p><small>{description}</small></p>
+          <button>Book:{name}</button>
         </div>
     );
 };
