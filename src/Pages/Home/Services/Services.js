08176bef3c2aa0d9c9517d699e45c386d1d57c54
Md Abbas Ali <mdabbasali185@gmail.com>
2022-04-12 12:15:14 +0600

    json generate

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2>
+                hellow
+            </h2>
+        </div>
+    );
+};
+
+export default Services;
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyC4WlA75PJrGgb9Uc8qvc1vXVz0DCbBKks",
   authDomain: "car-services-6b23d.firebaseapp.com",
@@ -13,9 +8,6 @@ const firebaseConfig = {
   messagingSenderId: "631074837578",
   appId: "1:631074837578:web:2b998aaca62f0178fd02b3",
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
 export default auth;
